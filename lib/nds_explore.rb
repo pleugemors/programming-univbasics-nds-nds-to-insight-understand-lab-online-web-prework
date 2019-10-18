be235b4829c1c4f80ac7063ca6a18d3c9509ff4f
Paul Leugemors <pleugemors@yahoo.com>
2019-10-18 19:48:46 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,6 +10,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  nds = directors_database
   number=0 
   while nds[0][:movies][number] do
     puts nds[0][:movies][number][:title]
