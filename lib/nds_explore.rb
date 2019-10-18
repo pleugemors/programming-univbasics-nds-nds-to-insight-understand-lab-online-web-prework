f42dcc098dc39700b892663b5375dddc55f0b46f
Paul Leugemors <pleugemors@yahoo.com>
2019-10-18 19:41:36 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -5,7 +5,7 @@ require 'directors_database'
 directors_database
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  puts nds[0][:movies][:title]
+  puts nds[0][:movies][0]
   pp nds
 end
 
